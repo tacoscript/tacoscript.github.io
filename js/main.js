2c246a23adfb4a105349e6b27652eca694fc0a21
Jordan Klassen <jordan@klassen.me.uk>
2015-11-27 00:03:23 -0800

    Clean up docster stuff

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@ $(document).ready(function(){
     $('#nav').onePageNav();
 
     $('a[href^="http"]').attr('target','_blank');
-    
+
     $('.toggle').click(function(){
         $('.overview').toggleClass('open');
     });
+
 });

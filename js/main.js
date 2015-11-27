b23ccd3e68f624c7b307580f6b9584aef82ed0b4
Jordan Klassen <jordan@klassen.me.uk>
2015-11-27 14:26:53 -0800

    Customize and fix style

--- js/main.js
+++ js/main.js
@@ -1,10 +1,43 @@
-$(document).ready(function(){
-    $('#nav').onePageNav();
+!function() {
 
-    $('a[href^="http"]').attr('target','_blank');
 
-    $('.toggle').click(function(){
-        $('.overview').toggleClass('open');
+    var $header = $('header:first');
+    var headerBottom;
+    var lastUpdate = -Infinity;
+
+    var $overview = $('.overview');
+    var $document = $(document);
+    var isFixed = false;
+    var adjustFixed = function() {
+        var now = +new Date();
+        var scrollTop = $document.scrollTop();
+        if (now - lastUpdate > 500) {
+            headerBottom = $header[0].getBoundingClientRect().bottom + scrollTop;
+            lastUpdate = now;
+        }
+        if (isFixed) {
+            if (scrollTop < headerBottom) {
+                isFixed = false;
+                $overview.removeClass('fixed');
+            }
+        } else {
+            if (scrollTop >= headerBottom) {
+                isFixed = true;
+                $overview.addClass('fixed');
+            }
+        }
+    };
+    adjustFixed();
+    $document.on('scroll', adjustFixed);
+
+    $document.ready(function(){
+        $('#nav').onePageNav();
+        $(window).trigger('scroll.onePageNav');
+
+        $('a[href^="http"]').attr('target','_blank');
+
+        $('.toggle').click(function(){
+            $('.overview').toggleClass('open');
+        });
     });
-
-});
+}();
